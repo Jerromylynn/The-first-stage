48c26306fab74b06a05c6aaf60f4a7835f3fddeb
Jerromy <1044685413@qq.com>
2017-10-18 14:39:48 +0800

    定义四舍五入函数
    c

--- 定义四舍五入函数.cpp
+++ 定义四舍五入函数.cpp
@@ -1,15 +1,12 @@
-N#include <stdio.h>
+#include <stdio.h>
 int main(){
 	double x,z;
-	int y,m;
+	int y;
 	printf("请输入一个数："); scanf("%lf\n",x); 
 	y=x;
 	z=x-y;
-	if (z>=0.5)
-	    m=y+1;
-	else
-	    m=y;
-	printf("四舍五入后的值为：%d\n",m);
-	
+	printf("整数部分为：%d\n",y);
+	printf("小数部分为：%d\n",z);
+
     return 0;	
 }
