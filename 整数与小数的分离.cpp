d52b20a4d5f3962261caa5258713ee8f61db2595
Jerromy <1044685413@qq.com>
2017-10-18 15:15:15 +0800

    整数与小数的分离

--- 整数与小数的分离.cpp
+++ 整数与小数的分离.cpp
@@ -1,12 +1,11 @@
-#include <stdio.h>
-int main(){
-	double x,z;
-	int y;
-	printf("请输入一个数："); scanf("%lf\n",x); 
-	y=x;
-	z=x-y;
-	printf("整数部分为：%d\n",y);
-	printf("小数部分为：%d\n",z);
-
-    return 0;	
-}
+  #include <stdio.h>
+  int main(){
+  	double x,z;
+  	int y;
+  	printf("输入一个数x：\n"); scanf("%lf",&x) ;
+  	y=x;
+  	z=x-y;
+  	printf("x的整数部分为：%d\n",y);
+  	printf("x的小数部分为：%lf\n",z);
+  	return 0;
+  } 
