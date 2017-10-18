00f71e7f15b1120043c4c368cdb7ca46562addb7
Jerromy <1044685413@qq.com>
2017-10-18 15:25:56 +0800

    求一元二次方程的解

---
+++ 求一元二次方程组的解1.cpp
@@ -0,0 +1,10 @@
+  #include <stdio.h>
+  int main(){
+  	double a,b,c,d,m,n,x,y;
+  	printf("输入x及y前的系数以及m,n的值：\n");
+  	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&m,&n);
+  	x=(m*d-b*n)/(a*d-b*c);
+  	y=(m*c-a*n)/(b*c-a*d);
+  	printf("x的值为：%lf\ny的值为：%lf\n",x,y);
+  	return 0;
+  } 
