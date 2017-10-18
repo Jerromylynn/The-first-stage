ad2b803885bee4836e34a09528affea5520c56da
Jerromy <1044685413@qq.com>
2017-10-18 15:47:54 +0800

    求几组数的最大公约数

---
+++ 求几组数的最大公约数.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+	int m,n;
+	int y;
+	int max,min;
+	int i,T;
+	
+	
+	printf(" ‰»Î    ‰≥ˆ\n");
+	printf("\n");scanf("%d",&T);
+	for(i=1;i<=T;i++){
+	
+	printf("");scanf("%d  %d",&m,&n);
+	
+	do{
+	
+	if(m>=n)
+	{
+	max=m;
+	min=n;}
+	
+	else {
+	 max=n;
+	      min=m;}
+	
+	y=max%min;
+	n=y;
+	m=min;
+	}while(n!=0);
+	printf("%d\n",m);
+}
+	return 0;
+}
