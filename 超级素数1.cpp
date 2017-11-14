686f3a9c9e679167a9c08fb5b49def921135fd5e
Jerromy <1044685413@qq.com>
2017-11-14 16:48:49 +0800

    作业纠正

--- 超级素数1.cpp
+++ 超级素数1.cpp
@@ -1,38 +1,40 @@
 #include<stdio.h>
 int isp(int n){
 	int i;
-	for(i=1;i<n;i++)
-	if (n%i==0)
-	return 0;
-	return 1; 
+	for(i=2;i<n;i++){
+	    if (n%i==0)
+	    return 0;
+		}
+	return 1;	
 }
+
 int main(){
-	int b,m,sum1=0,op=1,sum2=0,summ1=0,summ2=0, x=0;
+	int b,m,x,s;
+	int sum1=0,pro=1,sum2=0;
+	int ssum1=0,ssum2=0;
 	
 	for(b=100;b<=9999;b++){
-	
-	
-	if(b-1000>=0)
-	m=4;
-	else m=3;
-	 
-	 int a[m];
-	 
-	for(int j=0;j<m;j++){
-		a[j]=(b%10);
-		b/=10;
-		sum1+=a[j];//2
-		op*=a[j];//3
-		sum2+=a[j]*a[j];//4
-	}
-	if(isp(b)==1 && isp(sum1)==1 && isp(op)==1 && isp(sum2)==1){ 
-	summ1+=1; 
-	summ2+=b;
-	x=b;
-	} 
-}
-    printf("超级素数个数为：%d",summ1);
-    printf("所有超级s素数之和为：%d",summ2);
-    printf("最大的超级素数为：%d",x);
+     	if(b-1000>=0)
+		 	m=4;
+    	else m=3;
+    
+		int a[4];
+	    s=b;
+    	for(int j=0;j<m;j++){
+		a[j]=s%10;
+		s/=10;
+		sum1+=a[j];//各位数之和 
+		pro*=a[j];//各位数之积 
+		sum2+=a[j]*a[j];//各位数平方和 
+    	}
+ 	if(isp(b)==1 && isp(sum1)==1 && isp(pro)==1 && isp(sum2)==1){ 
+    	ssum1+=1; //超级素数个数 
+	    ssum2+=b;//所有超级素数和 
+    	x=b;//最大超级素数 
+    	} 
+    }
+    printf("超级素数个数为：%d\n",ssum1);
+    printf("所有超级素数之和为：%d\n",ssum2);
+    printf("最大的超级素数为：%d\n",x);
     return 0; 
 } 
